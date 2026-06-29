c3910bc967456428fd33d341cedd821a65ffc860
Musiimenta Tendo Divine <divinepraise699@gmail.com>
2026-06-29 15:51:16 +0300

    machine learning introduction

---
+++ Week 4/Day 1/Machine learning/intorduction.py
@@ -0,0 +1,56 @@
+#machine learning introduction
+#What is machine learning?
+#Machine learning is a subset of artificial intelligence (AI) that focuses on the development of algorithms and statistical models that enable computers to perform tasks without explicit instructions. 
+# Instead, these systems learn from data and improve their performance over time.
+
+#Types of Machine Learning
+#1. Supervised Learning
+#2. Unsupervised Learning
+#3. Reinforcement Learning
+
+# Supervised Learning
+# In supervised learning, the model is trained on a labeled dataset, which means that each training example is paired with an output label. 
+# The goal is for the model to learn the mapping from inputs to outputs so that it can make predictions on new, unseen data.
+
+# Unsupervised Learning
+# In unsupervised learning, the model is trained on an unlabeled dataset, meaning that there are no output labels provided.
+#  The goal is to find hidden patterns or intrinsic structures in the input data. 
+# Common techniques include clustering and dimensionality reduction.
+
+# Reinforcement Learning
+# In reinforcement learning, an agent learns to make decisions by interacting with an environment.  
+# The agent receives feedback in the form of rewards or penalties based on its actions, and it aims to maximize the cumulative reward over time.    
+
+#Roles of Machine Learning
+#1. Data Scientist
+#2. Machine Learning Engineer
+#3. Research Scientist
+#4. Business Analyst
+#5. AI/ML Product Manager
+#6. Data Engineer
+#7. Statistician
+#8. Software Engineer
+#9. Domain Expert
+#10. AI/ML Consultant
+#11. AI/ML Researcher
+#12. AI/ML Trainer
+#13. AI/ML Architect
+#14. AI/ML Operations Engineer
+#15. AI/ML Ethics Specialist
+#16. AI/ML Project Manager
+#17. Business Intelligence Analyst  
+#18. AI/ML Strategist
+#19. Data Architect
+#20. AI/ML Policy Advisor
+
+#Applications of Machine Learning
+#1. Image and Speech Recognition    
+#2. Natural Language Processing (NLP)
+#3. Recommendation Systems
+#4. Fraud Detection
+#5. Autonomous Vehicles
+#6. Healthcare and Medical Diagnosis    
+#7. Predictive Analytics
+#8. Customer Segmentation
+#9. Financial Market Analysis
+#10. Robotics
