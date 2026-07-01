1836d13f101b3558ef77c3b944537c4daf7f9faa
Musiimenta Tendo Divine <divinepraise699@gmail.com>
2026-07-01 11:04:52 +0300

    data visualization using matplotlib

---
+++ Week 4/Day 2/Data visualization.py
@@ -0,0 +1,46 @@
+#Matplotlib
+#Matplotlib is a comprehensive library for creating static, animated, and interactive visualizations in Python. 
+# It is widely used for data visualization and provides a variety of plotting functions to create different types of graphs and charts.   
+
+#How to install Matplotlib
+# You can install Matplotlib using pip, the Python package manager. 
+# Open your command prompt or terminal and run the following command:
+# pip install matplotlib
+
+#Importing Matplotlib
+# To use Matplotlib in your Python code, you need to import the library.
+
+#how to verify installation of matplotlib
+# After installing Matplotlib, you can verify the installation by running a simple test code.   
+#the command to verify the installation is:
+import matplotlib
+print(matplotlib.__version__)
+
+#matplotlib pyplot 
+# is a module in Matplotlib that provides a MATLAB-like interface for creating visualizations. 
+#it is commonly imported as plt and offers a wide range of functions for creating various types of plots and customizing their appearance.  
+#it simplifies the process of creating visualizations by providing a high-level interface for plotting data.    
+
+#steps to use pyplot
+#import matplotlib.pyplot as plt
+#create data e.g x = [1, 2, 3, 4, 5]
+#plot the data using plt.plot(x)    
+#customize the plot with labels, titles, and other visual elements using functions like plt.xlabel(), plt.ylabel(), plt.title(), etc.   
+#display the plot using plt.show()  
+
+#its importance is that it allows for the creation of high-quality visualizations to better understand and communicate data insights.
+
+#its functions include creating line plots, bar charts, scatter plots, histograms, and more. It also provides customization options for colors, labels, titles, and other visual elements.  
+
+#why is prefered over other libraries is that it is highly flexible, widely adopted, and has a large community of users and contributors. 
+# It integrates well with other libraries like NumPy and Pandas, making it a popular choice for data analysis and visualization tasks.  
+
+#Plotting Graphs
+#graphs can be plotted using the plot() function in Matplotlib.
+
+#Charts and Visualization
+# Matplotlib provides various types of charts and visualizations, including line plots, bar charts, scatter plots, histograms, pie charts, and more.    
+#charts can be customized with different colors, markers, line styles, labels, titles, and legends to enhance the visual representation of data.    
+#visualizations can be saved as image files in different formats (e.g., PNG, JPEG, PDF) using the savefig() function.   
+
+
